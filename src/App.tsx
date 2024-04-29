983f6c511833bcf626647bec49053e04ffc4a505
parkSeryu <pjh971212@gmail.com>
2024-04-29 22:21:20 +0900

    TypeScript 필수문법 10분 정리와 설치 셋팅 with React

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,41 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let type : string = 'kim';
+  let type0 : string[] = ['kim', 'park'];
+  let type1 : {name? : string} = {name : 'kim'}
+  let type2 : string | number  = 'kim'; // Union type
+
+  type 내타입 = string | number;
+
+  let 이름 : 내타입 = 123;
+
+  function 함수(x : number) : number {
+    return x * 2
+  }
+
+  type Member = [number, boolean]; // 튜플 타입
+  let john:Member = ['123', true];
+
+  type ObjectMember = {
+    [key: string] : string // 모든 Object 속성 
+  }
+
+  let johnson : ObjectMember = { name : 'kim', age : '123'}
+
+  class User{
+    name: string
+    constructor(name: string){
+      this.name = name;
+    }
+  }
+
+
+
+
+
+  
   return (
     <div className="App">
       <header className="App-header">
