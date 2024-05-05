543ad12790925ed9c5137b58ac4207552f648a23
parkSeryu <pjh971212@gmail.com>
2024-05-05 19:43:50 +0900

    class 만들 때 타입지정 가능

--- src/App.tsx
+++ src/App.tsx
@@ -2,59 +2,52 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Car{
+  model;
+  price;
+  constructor(a:string, b:number){
+    this.model = a;
+    this.price = b;
+  }
+
+  tax = () => {
+    return this.price / 10
+  }
+}
+
+class Word{
+  num:number[] = [];
+  str:string[] = [];
+
+  constructor(...x : (number | string)[]){
+    x.map(ele => {
+      if(typeof ele === 'number'){
+        this.num.push(ele)
+      }else{
+        this.str.push(ele)
+      }
+    })
+  }
+
+}
+
+let obj = new Word('kim', 3, 5, 'park');
+console.log(obj.num) //[3,5]
+console.log(obj.str) //['kim', 'park']
+
+let car1 = new Car('소나타', 3000)
+console.log(car1) //콘솔창 출력결과는 { model : '소나타', price : 3000 }
+console.log(car1.tax()) //콘솔창 출력결과는 300
+
+
 function App() {
 
-  type 함수타입 = (a: string) => number;
-
-  let 함수:함수타입 = function () {
-    return 10;
+  class Person {
+    data = 0;
   }
 
-  type 숙제1 = {
-    name : string,
-    age: number,
-    plusOne(x : number) : number,
-    changeName : () => void
-  }
-
-  let 회원정보 : 숙제1 = {
-    name : 'kim',
-    age: 30,
-    plusOne(a : number) : number {
-      return a + 1
-    },
-    changeName: () => {
-      console.log('안녕')
-    }
-  }
-  
-  회원정보.plusOne(2);
-
-  
-  type CutZeroType = (x:string) => string;
-
-  const cutZero : CutZeroType = (x) => {
-    if(x.charAt(0) === '0'){
-      x = x.slice(1, x.length);
-    }
-
-    return x;
-  }
-
-  type ZeroDashType = (x: string) =>  number;
-
-  const removeDash: ZeroDashType = (x) => {
-    if(x.includes('-')){
-      x = x.replaceAll('-','');
-    }
-    return parseInt(x);
-  }
-
-  const 만들함수 = (t1:string, t2:CutZeroType, t3:ZeroDashType) => {
-    console.log(t3(t2(t1)));
-  }
-
-  만들함수('010-1111-2222', cutZero, removeDash)
+  let 사람1 = new Person();
+  let 사람2 = new Person();
 
 
 
