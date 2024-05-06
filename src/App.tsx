367d2ec43c2134eb75affa654f3531baa9c06425
parkSeryu <pjh971212@gmail.com>
2024-05-06 19:18:59 +0900

    함수에 사용하는 never 타입도 있긴 합니다

--- src/App.tsx
+++ src/App.tsx
@@ -4,36 +4,19 @@ import './App.css';
 
 function App() {
 
-  type Fish = {swim : string}
-  type Bird = {fly : string}
-
-  function 함수(animal : Fish | Bird){
-    if('swim' in animal) {
-      animal.swim
+  function 함수(parameter : string){
+    if(typeof parameter == 'string'){
+      console.log(parameter)
+    }else{
+      console.log(parameter) // never
     }
-  }
 
-  let 날짜 = new Date();
-  if (날짜 instanceof Date){
-
-  }
-  
-  type Car = {
-    model : 4;
-    wheel : '4개',
-    color: string
-  }
-
-  type Bike = {
-    wheel : '2개',
-    color: string
-  }
-
-  function 함수(x: Car | Bike){
-    if(x.wheel === '4개'){
-      console.log('x는 Car 타입이에요')
+    let 함수 = function() {
+      throw new Error()
     }
+
   }
+
 
 
   return (
