b151e9ea2345f17e73fb864ea63566a9cdc1da90
parkSeryu <pjh971212@gmail.com>
2024-05-14 22:58:10 +0900

    조건문으로 타입만들기 & infer

--- src/test.ts
+++ src/test.ts
@@ -1,24 +1,25 @@
-type Car = {
-    color : boolean,
-    model : boolean,
-    price : boolean | number
-}
+type Age<T> = T extends string ? string : unknown
+let a: Age<string>
 
-type TypeChanger<MyType> = {
-    [key in keyof MyType] : string
-};
+type FirstItem<T> = T extends any[] ? T[0] : any;
 
-type 새로운타입 = TypeChanger<Car>
+type Person<T> = T extends infer R ? R : unknown
 
-type Bus = {
-  color : string,
-  model : boolean,
-  price : number
-}
+type a = Person<string>
 
-type TypeChanger <MyType, T> = {
-  [key in keyof MyType]: T;
-};
+type 타입추출<T> = T extends (infer R)[] ? R : unknown
+type a = 타입추출<string[]>;
 
-type NewBus = TypeChanger<Bus, boolean>;
-type NewBus2 = TypeChanger<Bus, string[]>
+type Age1<T> = T extends (infer R)[] ? T[0] extends string ? string : unknown;
+
+let age1 :Age1<[string, number]>;
+let age2 :Age1<[boolean, number]>; 
+
+
+type Age<T> = T extends [string, ...any] ? T[0] : unknown;
+let age1 :Age<[string, number]>;
+let age2 :Age<[boolean, number]>;
+
+type 타입뽑기<T> = T extends ( ()=> infer R ) ? R : any; 
+type a2 = 타입뽑기< (x:number) => void > // NewType은 number 타입입니다 
+type b = 타입뽑기<(x :string) => void>
